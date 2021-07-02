e06dbc0b2a6c7910a76b24601d87a1bdeb6e4436
Gabriel Martins <martinsgabriel1956@gmail.com>
2021-07-02 16:24:39 -0300

    feat: using Async/Await

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,35 +1,37 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-import { Section, Button } from './styles';
+import { Section, Button } from "./styles";
 
-import { MoviesList } from '../../components/MoviesList';
+import { MoviesList } from "../../components/MoviesList";
 
 export function Home() {
   const [movies, setMovies] = useState([]);
 
-  function handleFetchMovies() {
-    fetch(`https://swapi.dev/api/films`).then(res => res.json()).then(data => {
-      const transformedMovies = data.results.map(movieData => {
-        const { episode_id, title, opening_crawl, release_date } = movieData;
+  async function handleFetchMovies() {
+    const res = await fetch(`https://swapi.dev/api/films`);
+    const data = await res.json();
 
-        return {
-          id: episode_id,
-          title,
-          openingText: opening_crawl,
-          releaseDate: release_date
-        };
-      })
-      setMovies(transformedMovies);
-  });
+    const transformedMovies = data.results.map((movieData) => {
+      const { episode_id, title, opening_crawl, release_date } = movieData;
+
+      return {
+        id: episode_id,
+        title,
+        openingText: opening_crawl,
+        releaseDate: release_date,
+      };
+    });
+    setMovies(transformedMovies);
   }
+  
   return (
     <>
-    <Section>
-      <Button onClick={handleFetchMovies}>Fetch Movies</Button>
-    </Section>
-    <Section>
-      <MoviesList movies={movies} />
-    </Section>
-  </>
+      <Section>
+        <Button onClick={handleFetchMovies}>Fetch Movies</Button>
+      </Section>
+      <Section>
+        <MoviesList movies={movies} />
+      </Section>
+    </>
   );
 }
